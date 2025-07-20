c7b6c4705cbe1daefd66d113c3daf4a3536fce3b
mtacuri036 <tacurimateo59@gmail.com>
2025-07-20 14:00:56 -0500

    NuevoFinal

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -127,6 +127,10 @@ router.post('/login', async (req, res) => {
   }
 
   if (!usuario) return res.status(404).json({ mensaje: 'Usuario no encontrado.' });
+
+  // NUEVA VALIDACIÓN
+  if (!usuario.activo) return res.status(403).json({ mensaje: 'Cuenta inactiva. Contacte al administrador.' });
+
   if (usuario.password !== password) return res.status(400).json({ mensaje: 'Contraseña incorrecta.' });
 
   res.json({
